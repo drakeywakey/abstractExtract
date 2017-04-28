fe67aa2b89548a0138f698e4c3aad53e3810054f
Drake Bennion <drakebennion@gmail.com>
2017-04-27 20:41:24 -0600

    just getting text submitted. Probably need to use ejs or something for rendering

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function (req, res) {
+	var body = req.body;
+	res.set('Content-Type', 'text/plain');
+	res.send(body.article);
 });
 
 app.listen(app.get('port'), function (){
